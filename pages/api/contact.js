b252b6e4ce8d6030320871abb91c7615969eccc2
wediddit <contact@wediddit.fr>
2026-04-21 13:41:07 +0200

    mail message

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -10,8 +10,8 @@ export default function handler(req, res) {
 
   // Création du message
   const sendGridMail = {
-    to: "contact@stdautomobile.com",
-    from: "contact@stdautomobile.com",
+    to: "std.automobile@gmail.com",
+    from: "wediddit.contact@gmail.com",
     templateId: "d-3de4eebe210140cbb6252aa19e2dafdf",
     dynamic_template_data: {
       name: name,
